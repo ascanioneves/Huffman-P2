43414ff1d887ea4834d1db4409203df073c33f9b
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2019-12-27 10:08:30 -0300

    Update Types.h

--- Huffman/Estruturas/Types.h
+++ Huffman/Estruturas/Types.h
@@ -1,5 +1,7 @@
 typedef struct huff_heap huff_heap;
 typedef struct huff_node huff_node;
+typedef struct hash hash ;
+typedef struct item item ;
 
 struct huff_node
 {
@@ -10,4 +12,15 @@ struct huff_node
 struct huff_heap 
 {
   huff_node *data[257];
+};
+struct item 
+{
+    void *element ;
+    unsigned int shift_bit ;
+    int bits ;
+    int frequencia ;
+};
+struct hash
+{
+    item *table[500] ;
 };

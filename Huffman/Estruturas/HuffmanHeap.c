dce7b16d45254040ea7e3e630ce18315c18bd77a
Ascanio Savio <ascaniosavio97@gmail.com>
2019-12-27 11:32:46 -0300

    Adding first huff heap

---
+++ Huffman/Estruturas/HuffmanHeap.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "HuffmanHeap.h"
+#include "Types.h"
+
+int is_empty(huff_heap *heap)
+{
+  return (heap->size == 0 || heap == NULL);
+}
+
+void swap(huff_heap *heap, int i, int j)
+{
+  huff_node *aux = heap->data[i];
+  heap->data[i] = heap->data[j];
+  heap->data[j] = aux;
+}
+
+int get_parent_index(huff_heap *heap, int i) { return (i / 2); }
+
+int get_left_index(huff_heap *heap, int i) { return (2 * i); }
+
+int get_right_index(huff_heap *heap, int i) { return (2 * i + 1); }
+
+huff_heap *create_heap()
+{
+  huff_heap *new_heap = (huff_heap *) malloc(sizeof(huff_heap));
+  new_heap -> size = 0;
+  int i;
+  for(i = 1; i < 257; i++)
+    new_heap -> data[i] = NULL;
+  return new_heap;
+}
+
+void min_heapify(huff_heap *heap, int i)
+{
+  int largest;
+  int left_index = get_left_index(heap, i);
+  int right_index = get_right_index(heap, i);
+  if(left_index <= heap->size && heap->data[left_index]->freq < heap->data[i]->freq)
+    largest = left_index;
+  else
+    largest = i;
+  
+  if(right_index <= heap->size && heap->data[right_index]->freq < heap->data[largest]->freq)
+    largest = right_index;
+  if(heap->data[i]->freq != heap->data[largest]->freq)
+  {
+    swap(heap, i, largest);
+    min_heapify(heap, largest);
+  }
+}
+
+void enqueue(huff_heap *heap, huff_node *node)
+{
+  if(heap -> size >= 256)
+  {
+    printf("Heap Overflow!\n");
+    return;
+  }
+  else
+  {
+    heap->data[++heap->size] = node;
+    int key_index = heap->size;
+    int parent_index = get_parent_index(heap, key_index);
+    while(parent_index >= 1 && heap->data[key_index]->freq < heap->data[parent_index]->freq)
+    {
+      swap(heap, key_index, parent_index);
+      key_index = parent_index;
+      parent_index = get_parent_index(heap, key_index);
+    }
+  } 
+}
+
+huff_node* dequeue(huff_heap *heap)
+{ 
+  if(is_empty(heap))
+  {
+    printf("Heap Underflow!\n");
+    return NULL;
+  }
+  else
+  {
+    huff_node *aux = heap->data[1];
+    heap->data[1] = heap->data[heap->size];
+    heap->size--;
+    min_heapify(heap, 1);
+    return aux;
+  }
+}
+
